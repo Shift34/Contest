9798ddc01e3b4a3a55217f289d2e88c30ac00f0a
Shift34 <116919465+Shift34@users.noreply.github.com>
2023-03-15 20:48:54 +0300

    Create 2_2_1_1.cs

---
+++ 2_2_1_1.cs
@@ -0,0 +1,98 @@
+Ваша задача осуществить обход графа методом "сначала вглубь" и вывести последовательность вершин графа в порядке обхода. 
+Программа должна быть написана с соблюдением принципов ООП (должен быть класс граф, класс алгоритма обхода).
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        public class Algorithm
+        {
+            public static Stack<Picks> stack = new Stack<Picks>();
+            public static string input;
+            public static int count;
+            public static void Run(Graph graph)
+            {
+                stack.Push(graph.piks[graph.elementary]);
+                graph.piks[graph.elementary].state = false;
+                input += graph.piks[graph.elementary].number + " ";
+                while (stack.Count > 0)
+                {
+                    count = 0;
+                    for (int i=0; i < stack.Peek().connection.Count; i++)
+                    {
+                        if (graph.piks[stack.Peek().connection[i]].state)
+                        {
+                            input += graph.piks[stack.Peek().connection[i]].number + " ";
+                            graph.piks[stack.Peek().connection[i]].state = false;
+                            stack.Push(graph.piks[stack.Peek().connection[i]]);
+                            count=0;
+                            i = -1;
+                        }
+                    }
+                    if (count == 0)
+                    {
+                        stack.Pop();
+                    } 
+                }
+                using (StreamWriter sw = new StreamWriter("output.txt"))
+                {
+                    sw.WriteLine(input);
+                } 
+            }
+        }
+
+        public class Graph
+        {
+            public Picks[] piks;
+            public string line;
+            public int elementary;
+            public Graph(string String)
+            {
+                StreamReader sr = new StreamReader(String);
+                line = sr.ReadLine();
+                int count = int.Parse(line);
+                piks = new Picks[int.Parse(line)];
+                for (int i = 0; i < count; i++)
+                {
+                    line = sr.ReadLine();
+                    string []input = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    Array.Sort(input);
+                    piks[i] = new Picks(input, i);
+                }
+                line = sr.ReadLine();
+                elementary = int.Parse(line);
+            }
+        }
+
+        public class Picks 
+        {
+            public bool state;
+            public List<int> connection;
+            public int number;
+            public Picks(string [] input,int number )
+            {
+                connection = new List<int>();
+                state = true;
+                for (int j = 0; j < input.Length; j++)
+                {
+                    connection.Add(int.Parse(input[j]));
+                }
+                this.number = number;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph("input.txt");
+            Algorithm.Run(graph);
+        }
+    }
+}
